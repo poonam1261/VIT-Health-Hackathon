58cff12d6f8c681c3a01bc025cdb03616f63bd74
poonam1261 <pprabhugaonkar6@nitgoa.ac.in>
2025-02-02 20:00:41 +0530

    Medical Dash

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,6 +7,8 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -30,14 +32,21 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={24} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="meddash"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Med Dash',
+          tabBarIcon: ({ color }) => <MaterialIcons name="dashboard" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="telemed"
+        options={{
+          title: 'TeleMed',
+          tabBarIcon: ({ color }) => <FontAwesome6 name="hand-holding-medical" size={24} color={color} />,
         }}
       />
     </Tabs>
