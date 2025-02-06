196827c5e7e145579325408ef3d0951330f6b03b
poonam1261 <pprabhugaonkar6@nitgoa.ac.in>
2025-02-06 14:13:51 +0530

    Firestore

--- app/Appointment/bookAppt.jsx
+++ app/Appointment/bookAppt.jsx
@@ -1,21 +1,70 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { ExpandableCalendar, CalendarProvider, AgendaList } from "react-native-calendars";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { ExpandableCalendar, CalendarProvider, AgendaList, Calendar } from "react-native-calendars";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
+import { db } from '../../firebase/firebaseConfig'
 
 const appointments = {
   "2025-02-06": [{ id: "1", hour: "10:00 AM", title: "Doctor Appointment" }],
   "2025-02-11": [
-    { id: "2", hour: "9:00 AM", title: "Gym Session" },
-    { id: "3", hour: "2:00 PM", title: "Business Meeting" },
+    { id: "2", hour: "9:00 AM", title: "Available" },
+    { id: "3", hour: "9:30 AM", title: "Not Available" },
+    { id: "4", hour: "10:00 AM", title: "Available" },
+    { id: "5", hour: "10:30 AM", title: "Available" },
+  ],
+  "2025-02-12": [
+    { id: "10", hour: "9:00 AM", title: "Available" },
+    { id: "11", hour: "9:30 AM", title: "Not Available" },
+    { id: "12", hour: "10:00 AM", title: "Available" },
+    { id: "13", hour: "10:30 AM", title: "Not Available" },
+  ],
+  "2025-02-13": [
+    { id: "6", hour: "9:00 AM", title: " Not Available" },
+    { id: "7", hour: "9:30 AM", title: "Not Available" },
+    { id: "8", hour: "10:00 AM", title: "Available" },
+    { id: "9", hour: "10:30 AM", title: "Available" },
   ],
 };
 
+
 const bookAppt = () => {
   const [selectedDate, setSelectedDate] = useState("2025-02-06");
+  const [selectedApptId, setsectedApptId] = useState(null);
 
   const handleDayPress = (day) => {
     setSelectedDate(day.dateString);
+    fetchAppointments();
+  }
+
+  const handleApptPress = (item) => {
+    setsectedApptId(item.id);
+   { /*if(item.title === "Not Available"){
+      alert("Slot is already booked")
+    }
+    item.title = "Appointment Booked"*/}
+  }
+  const getAppointmentStyle = (apptId) => ({
+    backgroundColor: selectedApptId === apptId ? "#fef2e4" : "rgba(0,0,0,0.2)",
+  });
+ 
+   // const db = firestore();
+
+   const fetchAppointments = async () => {
+    try {
+      const snapshot = await getDocs(collection(db, "Appointments"));
+      const appointments = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      console.log("Appointments:", appointments);
+      return appointments;
+    } catch (error) {
+      console.error("Error fetching appointments:", error);
+    }
   };
+
+  
+  
 
   // Mark selected date with a green circle
   const markedDates = {
@@ -28,7 +77,7 @@ const bookAppt = () => {
 
   return (
     <CalendarProvider date={selectedDate}>
-      <ExpandableCalendar 
+      <Calendar 
         onDayPress={handleDayPress}
         markedDates={markedDates} 
         hideArrows={false} 
@@ -44,10 +93,10 @@ const bookAppt = () => {
             },
           ]}
           renderItem={({ item }) => (
-            <View style={styles.appointmentItem}>
+            <TouchableOpacity style={[styles.appointmentItem , getAppointmentStyle(item.id)]} onPress={() => {handleApptPress(item)}}>
               <Text style={styles.time}>{item.hour}</Text>
               <Text style={styles.title}>{item.title}</Text>
-            </View>
+            </TouchableOpacity>
           )}
         />
         {(!appointments[selectedDate] || appointments[selectedDate].length === 0) && (
@@ -72,6 +121,8 @@ const styles = StyleSheet.create({
     padding: 10,
     borderBottomWidth: 1,
     borderBottomColor: "#ddd",
+    backgroundColor:'rgba(0,0,0,0.2)', 
+    marginBottom:4
   },
   time: {
     fontSize: 16,
